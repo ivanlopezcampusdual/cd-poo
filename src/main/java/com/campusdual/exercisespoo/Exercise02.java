563d4d08c48649704aa886e56281f55d8bdb504e
ivanlopezcampusdual <ivan.lopez@campusdual.com>
2023-10-19 10:02:10 +0200

    add exercise 2

---
+++ src/main/java/com/campusdual/exercisespoo/Exercise02.java
@@ -0,0 +1,24 @@
+package com.campusdual.exercisespoo;
+
+public class Exercise02 {
+
+    public static void main(String[] args) {
+        // Área de un círculo = PI * r * r
+        // Longitud de una circunferencia = PI * 2 * r
+        // Área de un cuadrado = lado * lado
+
+       double r = 7;
+       double pi = Math.PI;
+       double l = 4;
+
+        System.out.print("Área del circulo: ");
+        System.out.println(pi*r*r);
+        System.out.print("Longitud de una circunferencia: ");
+        System.out.println(pi*2*r);
+        System.out.print("Área de un cuadrado: ");
+        System.out.println(l*l);
+
+
+    }
+
+}
